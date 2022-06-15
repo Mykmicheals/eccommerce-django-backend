78e61c9fbc7e1aed7f480e78a01d5d75c7c9eb61
mykmicheals <mykmicheals@gmail.com>
2022-06-15 12:48:54 +0100

    jwt-auth

--- userAuthentication/urls.py
+++ userAuthentication/urls.py
@@ -8,7 +8,7 @@ from .views import RegisterView,MyTokenObtainPairView
 
 urlpatterns = [
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-       path('api/token/',MyTokenObtainPairView, name='token_obtain_pair'),
+       path('api/token/',MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
 ]
--- userAuthentication/views.py
+++ userAuthentication/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer,MyTokenObtainPairSerializer
 from rest_framework import generics
 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer  
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
@@ -12,14 +10,8 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-# class CustomTokenObtainPairView(TokenObtainPairView):
-#     # Replace the serializer with your custom
-#     serializer_class = CustomTokenObtainPairSerializer
